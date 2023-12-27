0fed22c01e45ffa1b92658b62e2b763820496e00
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-27 20:30:13 +0800

    added types

--- main.go
+++ main.go
@@ -9,6 +9,22 @@ import (
 	"os"
 )
 
+type ytType struct {
+	Title            string `json:"title"`
+	Author_name      string `json:"author_name"`
+	Author_url       string `json:"author_url"`
+	Type             string `json:"type"`
+	Height           string `json:"height"`
+	Width            string `json:"width"`
+	Version          string `json:"version"`
+	Provider_name    string `json:"provider_name"`
+	Provider_url     string `json:"provider_url"`
+	Thumbnail_height string `json:"thumbnail_height"`
+	Thumbnail_width  string `json:"thumbnail_width"`
+	Thumbnail_url    string `json:"thumbnail_url"`
+	Html             string `json:"html"`
+}
+
 func main() {
 	data, err := http.Get("https://www.youtube.com/oembed?url=" + "https://www.youtube.com/watch?v=uvb-1wjAtk4" + "&format=json")
 	if err != nil {
